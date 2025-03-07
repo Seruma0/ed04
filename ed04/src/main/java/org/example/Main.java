6a217bf8849f27eb7a0e68f0679c0d6fd8b90222
Seruma0 <409012@alu.murciaeduca.es>
2025-03-07 13:38:39 +0100

    Creamos el metodo initAgenda que inicializa tres contactos dados.

--- ed04/src/main/java/org/example/Main.java
+++ ed04/src/main/java/org/example/Main.java
@@ -6,10 +6,8 @@ public class Main {
         // Crear una nueva agenda.
         Agenda agendaContactos= new Agenda();
 
-        // Añadir contactos a la agenda.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        //Iniciamos la agenda de contactos.
+        initAgenda(agendaContactos);
 
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
@@ -30,5 +28,11 @@ public class Main {
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+    private static void initAgenda(Agenda agendaContactos){
+        // Añadir contactos a la agenda.
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
     }
 }
